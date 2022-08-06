f47c0211e4e401374497a20d2a3b37c6fc45ebb9
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-08-06 14:48:19 +0300

    Multi-thread Youtube Watcher with links Array and Mouse Robot on Selenium

--- src/main/java/Liker.java
+++ src/main/java/Liker.java
@@ -2,154 +2,139 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.util.ArrayList;
 
 public class Liker {
     public static void main(String[] args) throws InterruptedException, AWTException {
-        System.setProperty("webDriver.chrome.driver", "Selenium\\chromedriver.exe"); //системная настройка
-        for (int i = 0; i < 10; i++) { //запускаем 10 потоков с интервалом 25 секунд - более 10 не тянет местный интернет
-            new Watcher().start();
+        ArrayList<String> address = new ArrayList<>();
+        address.add("https://www.youtube.com/watch?v=yMZozCGKo7A");// Пранаямы (47);
+        address.add("https://www.youtube.com/watch?v=ykjToGfSzOk");// Слим марафон 3 (52);
+        address.add("https://www.youtube.com/watch?v=R0T_a5p7Rps");// Слим марафон 5 (52)
+        address.add("https://www.youtube.com/watch?v=oDhpQr4J-wY");// Слим марафон 6 (60);
+        address.add("https://www.youtube.com/watch?v=1ljV4dqaSPA");// Гимнастика для глаз (72);
+        address.add("https://www.youtube.com/watch?v=Kde1-uKhhBU");// Утренняя разминка (46);
+        address.add("https://www.youtube.com/watch?v=5ssF3AM4Kd4");// Силовой комплекс на ноги и ягодицы;
+        address.add("https://www.youtube.com/watch?v=XKiXtOOhBQ4");// Слим марафон 8 (52);
+        address.add("https://www.youtube.com/watch?v=J3P_thgvzvI");// Слим марафон 3 (53);
+        address.add("https://www.youtube.com/watch?v=thmBZFe-XTU");// Сурья намаскар (36);
+        address.add("https://www.youtube.com/watch?v=oNAMGepDjqk");// Слим марафон (53);
+        address.add("https://www.youtube.com/watch?v=ffXjSOJDXKU");// Работа со стопой (78);
+        address.add("https://www.youtube.com/watch?v=xt5TpDD7VO8");// Пресс (44);
+        address.add("https://www.youtube.com/watch?v=Sezu8ZSULfM");// Пранаямы д/реабилитации (42);
+        address.add("https://www.youtube.com/watch?v=pmLHLeZ5Mm8");// Слим марафон 2 (45);
+        address.add("https://www.youtube.com/watch?v=U94P_QV8sek");// Комплекс раскрытия ТЗБ (41);
+        address.add("https://www.youtube.com/watch?v=qZj854lwJYU");// Пранаямы (51);
+        address.add("https://www.youtube.com/watch?v=D3t5bbUIoBg");// Зарядка 3 (52);
+        address.add("https://www.youtube.com/watch?v=s1wNLmQHpZE");// Сбалансированная практика (57);
+        address.add("https://www.youtube.com/watch?v=r9fcijBHgoM");// Йога для шеи (45);
+        address.add("https://www.youtube.com/watch?v=YtDcSCsy9Yg");// Йога на спину (57);
+        address.add("https://www.youtube.com/watch?v=xhpr5Qs1_ZA");// Миофасциальный релиз (64);
+        address.add("https://www.youtube.com/watch?v=PReP97oAjfo");// Приветствие луне (65);
+        address.add("https://www.youtube.com/watch?v=ufAeUNZWgW4");// Йога для спины (66);
+        address.add("https://www.youtube.com/watch?v=oDhpQr4J-wYh");// Слим марафон 6 (60);
+        address.add("https://www.youtube.com/watch?v=81r6eGhtf5M");// Пранаямы для восстановления после болезни.
+        address.add("https://www.youtube.com/watch?v=sDEFWTcMj2E");// Раскрытие грудного отдела (54);
+        address.add("https://www.youtube.com/watch?v=jB5s5Z9Kxxc");// Самаконасана (65);
+        address.add("https://www.youtube.com/watch?v=wr21pXdJ2KE");// Массаж лица (68);
+        address.add("https://www.youtube.com/watch?v=IAxpyRVSSFs");// Утренняя зарядка (68);
+        address.add("https://www.youtube.com/watch?v=aeX7ibapPfY");// Суставная гимнастика (55);
+        address.add("https://www.youtube.com/watch?v=ZVwHbA4_GuA");// Slim marathon (69);
+        address.add("https://www.youtube.com/watch?v=NMX6vigpZfc");// Утренняя зарядка 2 (99);
+        address.add("https://www.youtube.com/watch?v=thmBZFe-XTU");// Сурья намаскар (36);
+        address.add("https://www.youtube.com/watch?v=xt5TpDD7VO8");// Пресс (44);
+        address.add("https://www.youtube.com/watch?v=Sezu8ZSULfM");// Пранаямы для реабилитации после вирусных заболеваний (42)
+        address.add("https://www.youtube.com/watch?v=U94P_QV8sek");// Комплекс для раскрытия тазобедренного сустава (41);
+        address.add("https://www.youtube.com/watch?v=Kde1-uKhhBU");// Утренняя разминка (46);
+
+        //System.setProperty("webDriver.chrome.driver", "Selenium\\chromedriver.exe"); //системная настройка
+        for (int i = 0; i < 10; i++) { //запускаем 10 потоков с интервалом 25 секунд
+            new Watcher(address).start();
             Thread.sleep(25000);
         }
     }
 
     public static class Watcher extends Thread {
+        ArrayList<String> addressMap;
+        public Watcher(ArrayList<String> address) {
+            this.addressMap = address;
+        }
 
         public void run() {
-            //super.run();
-            //System.setProperty("webdriver.chrome.driver", "Selenium\\chromedriver.exe");
-            //for (int i = 0; i < 20; i++) {
+            super.run();
+
             WebDriver driver = new ChromeDriver();
-            driver.navigate().to("https://www.youtube.com/watch?v=oAYOzRwtDg4&list=UUMOYFUwyTHCggfXitcD7ciA&index=1");//Канал Yoga_FreshBar
+            driver.get("https://www.youtube.com/watch?v=yMZozCGKo7A"); //открываем первый ролик
             driver.manage().window().maximize();
             try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                synchronized (Watcher.class) {
-                    pressingButtons();
-                }
+                Liker.pressingButtons(); // Нажимаем нужные кнопки для старта
             } catch (AWTException e) {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            //System.out.println(Thread.currentThread() + "запустил цикл " + (i + 1));
-
-            Robot robot = null; //создаём робота управления мышкой
-            try {
-                robot = new Robot();
-            } catch (AWTException e) {
-                throw new RuntimeException(e);
-            }
-            String address = "https://www.youtube.com/watch?v=Q0PLP8qyLEU&list=UUMOYFUwyTHCggfXitcD7ciA&index=";
-            for (int j = 0; j < 41; j++) { //цикл на 41 видео
-                System.out.println(Thread.currentThread().getName() + " смотрит ролик № " + (j + 1));
-                driver.manage().window().minimize(); //прячем экран
+            int index;
+            while (true) { //запускаем бесконечный цикл
+                index = (int) (Math.random()* addressMap.size());
+                driver.get(addressMap.get(index)); //запускаем видео
+                driver.manage().window().minimize(); //прячем окно
+                System.out.println(Thread.currentThread().getName() + " смотрит ролик "+ index);
                 try {
-                    Thread.sleep(60000); //смотрим ролик 60 секунд
+                    Thread.sleep(90000); //смотрим ролик 90 секунд
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    continue;
                 }
-                driver.manage().window().maximize(); //возвращаем экран
-                driver.navigate().to(address + (j + 1)); //включаем следующее видео на канале
-//                    try {
-//                        Thread.sleep(500);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    synchronized (Watcher.class) {
-//                        robot.mouseMove(240, 660); //Следующее видео
-//                        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-//                        try {
-//                            Thread.sleep(500);
-//                        } catch (InterruptedException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
             }
-
-            System.out.println(Thread.currentThread().getName() + "насмотрелся");
-            driver.quit(); //закончили просмотр всех роликов на канале
-            this.interrupt();
         }
     }
-    //}
 
     public static synchronized void pressingButtons() throws AWTException, InterruptedException {
         Robot robot1 = new Robot(); //создаём робота
-        //System.out.println("робот создан");
-        robot1.mouseMove(1080, 720);
-        Thread.sleep(400);
+
+        robot1.mouseMove(1085, 720);
+        Thread.sleep(1000);
         robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //сползаем вниз
         //System.out.println("скролим вниз");
-        Thread.sleep(400);
+        Thread.sleep(500);
         robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        Thread.sleep(400);
 
-        robot1.mouseMove(990, 740); //принять все
-        //System.out.println("Принять всё");
+        robot1.mouseMove(1040, 740); //принять все
+        Thread.sleep(1000);
         robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         Thread.sleep(400);
         robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        Thread.sleep(1000);
-
-//        robot1.mouseMove(370, 370);
-//        Thread.sleep(3000);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //воспроизвести всё
-//        //System.out.println("Воспроизвести всё");
-//        Thread.sleep(400);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-//        robot1.mouseMove(190, 600);
-//        Thread.sleep(1500);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //переходим по рекламе
-//        //System.out.println("Выключаем звук");
-//        Thread.sleep(400);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-
-        robot1.mouseMove(280, 660);
         Thread.sleep(2000);
+
+        robot1.mouseMove(235, 670);
+        Thread.sleep(1000);
         robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //выключаем звук
         //System.out.println("Выключаем звук");
-        Thread.sleep(400);
+        Thread.sleep(500);
         robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        //Thread.sleep(30000); //пауза на рекламу 30 секунд
 
-//        robot1.mouseMove(280, 30);
-//        Thread.sleep(1500);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //открываем новую вкладку
-//        System.out.println("открываем новую вкладку");
-//        Thread.sleep(500);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-//
-//        robot1.mouseMove(500, 640);
-//        Thread.sleep(1500);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //переходим в настройки
-//        System.out.println("переходим в настройки");
-//        Thread.sleep(500);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        robot1.mouseMove(755, 670);
+        Thread.sleep(1000);
+        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //переходим в настройки
+        //System.out.println("переходим в настройки");
+        Thread.sleep(500);
+        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-//        robot1.mouseMove(500, 510);
-//        Thread.sleep(1000);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //переходим к выбору скорости
-//        Thread.sleep(500);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-//
-//        robot1.mouseMove(660, 600);
-//        Thread.sleep(1000);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //двигаем ползунок вниз
-//        Thread.sleep(500);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-//
-//        robot1.mouseMove(630, 580);
-//        Thread.sleep(1000);
-//        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //выбираем двойную скорость
-//        Thread.sleep(500);
-//        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        robot1.mouseMove(755, 520);
+        Thread.sleep(1000);
+        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //переходим к выбору скорости
+        Thread.sleep(500);
+        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        robot1.mouseMove(965, 600);
+        Thread.sleep(1000);
+        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //двигаем ползунок вниз
+        Thread.sleep(500);
+        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        robot1.mouseMove(900, 600);
+        Thread.sleep(1000);
+        robot1.mousePress(InputEvent.BUTTON1_DOWN_MASK); //выбираем двойную скорость
+        Thread.sleep(500);
+        robot1.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 }
 
